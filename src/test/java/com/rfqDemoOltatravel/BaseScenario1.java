46cbe66301330aedf98cc631d9361a4daa43a840
IgnatenkoPavlo <ignatenko.pavlo@yandex.ru>
2017-09-28 00:07:59 +0300

    Написание базового сценария

--- src/test/java/com/rfqDemoOltatravel/BaseScenario1.java
+++ src/test/java/com/rfqDemoOltatravel/BaseScenario1.java
@@ -3,12 +3,15 @@ package com.rfqDemoOltatravel;
 import com.codeborne.selenide.WebDriverRunner;
 import org.apache.bcel.generic.RETURN;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -95,6 +98,15 @@ public class BaseScenario1 {
         //Ждём пока страница прогрузится
         $(By.xpath("//span[contains(text(),'Loading')]")).shouldNot(exist);
 
+        Double rubUsd = 0.0;
+        rubUsd = Double.valueOf($(By.cssSelector("table[id=\"table-options\"] tr[data-key=\"rub_usd_rate\"] td[class=\"value editable editable-quotatoin-option-value\"]")).getText());
+        System.out.println(rubUsd);
+
+        Double generalMarge = 0.0;
+        generalMarge = Double.valueOf(($(By.cssSelector("table[id=\"table-options\"] tr[data-key=\"general_marge\"] td[class=\"value editable editable-quotatoin-option-value\"]")).getText()).replace(',', '.'));
+        System.out.println(generalMarge);
+
+
         //Меняем колличество ночей на 3
         $(By.cssSelector("table[id=\"table-options\"] tr[data-key=\"number_of_nights\"] td[class=\"value editable editable-quotatoin-option-value\"]")).click();
         $(By.cssSelector("table[id=\"table-options\"] tr[data-key=\"number_of_nights\"] td[class=\"value editable editable-quotatoin-option-value\"]")).setValue("3");
@@ -143,90 +155,95 @@ public class BaseScenario1 {
         priceDBL = $(By.xpath("//div[@id=\"modal-accommodation-days-prices\"]//div[@class=\"modal-body\"]/table/tfoot/tr[1]/th[3]")).getText();
         System.out.println(priceDBL);
         Double priceDBLD = Double.valueOf(priceDBL);
+        priceDBLD = priceDBLD / 2;
         System.out.println(priceDBLD);
         //Закрываем модальное окно
         $(By.cssSelector("div[id=\"modal-accommodation-days-prices\"] button[class=\"btn btn-primary\"]")).click();
 
 
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tfoot//a[@class=\"qbtn qbtn-showallprices\"]")).scrollTo();
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tfoot//a[@class=\"qbtn qbtn-showallprices\"]")).click();
         Double programFor15 = 0.0;
         Double programFor20 = 0.0;
         Double programFor25 = 0.0;
-        //$(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][3]//tr[@class=\"service\"][2]"));
 
-        //System.out.println($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[1]//span[@class=\"sum\"]")).getText());
-        programFor15 = programFor15 +
-                Double.valueOf($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[1]//span[@class=\"sum\"]")).getText());
+        //Считаем суммы для 3-х групп: 15, 20, 25
+        for (int dayCounter = 1; dayCounter <= 4; dayCounter++) {
 
-        //System.out.println($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[2]//span[@class=\"sum\"]")).getText());
-        programFor20 = programFor20 +
-                Double.valueOf($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[2]//span[@class=\"sum\"]")).getText());
+            $(By.xpath(ProgrammSection.GetADay(dayCounter) + "//tfoot//a[@class=\"qbtn qbtn-showallprices\"]")).scrollTo();
+            $(By.xpath(ProgrammSection.GetADay(dayCounter) + "//tfoot//a[@class=\"qbtn qbtn-showallprices\"]")).click();
 
-        //System.out.println($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[3]//span[@class=\"sum\"]")).getText());
-        programFor25 = programFor25 +
-                Double.valueOf($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[3]//span[@class=\"sum\"]")).getText());
+            for (int serviceCounter = 1; serviceCounter <= 3; serviceCounter++) {
+                $(By.xpath(ProgrammSection.GetADay(dayCounter) + "//tr[@class=\"service\"][" + String.valueOf(serviceCounter) + "]"
+                        + ProgrammSection.GetSumForUnit(1))).scrollTo();
+                programFor15 = programFor15 +
+                        Double.valueOf($(By.xpath(ProgrammSection.GetADay(dayCounter) + "//tr[@class=\"service\"][" + String.valueOf(serviceCounter) + "]"
+                                + ProgrammSection.GetSumForUnit(1))).getText());
+
+                programFor20 = programFor20 +
+                        Double.valueOf($(By.xpath(ProgrammSection.GetADay(dayCounter) + "//tr[@class=\"service\"][" + String.valueOf(serviceCounter) + "]"
+                                + ProgrammSection.GetSumForUnit(2))).getText());
+
+                programFor25 = programFor25 +
+                        Double.valueOf($(By.xpath(ProgrammSection.GetADay(dayCounter) + "//tr[@class=\"service\"][" + String.valueOf(serviceCounter) + "]"
+                                + ProgrammSection.GetSumForUnit(3))).getText());
+
+            }
+
+            $(By.xpath(ProgrammSection.GetADay(dayCounter) + "//tfoot//a[@class=\"qbtn qbtn-hideallprices\"]")).click();
+        }
         System.out.println(programFor15 + " " + programFor20 + " " + programFor25);
 
 
-        //System.out.println($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[1]//span[@class=\"sum\"]")).getText());
-        programFor15 = programFor15 +
-                Double.valueOf($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][2]//td[@class=\"featureds\"]//tbody//tr[1]//span[@class=\"sum\"]")).getText());
+        //Запускаем Расчёт
+        $(By.id("qbtn-execute")).scrollTo();
+        $(By.id("qbtn-execute")).click();
+        $(By.xpath("//span[contains(text(),'Loading')]")).shouldNot(exist);
 
-        //System.out.println($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[2]//span[@class=\"sum\"]")).getText());
-        programFor20 = programFor20 +
-                Double.valueOf($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][2]//td[@class=\"featureds\"]//tbody//tr[2]//span[@class=\"sum\"]")).getText());
+        //Сравниваем цену за номер
+        /*Assert.assertEquals(new BigDecimal(priceDBLD).setScale(0, RoundingMode.HALF_UP).floatValue(),
+                );*/
+        $(By.id("table-result-hotels-wo-margin-we")).scrollTo();
 
-        //System.out.println($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[3]//span[@class=\"sum\"]")).getText());
-        programFor25 = programFor25 +
-                Double.valueOf($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][2]//td[@class=\"featureds\"]//tbody//tr[3]//span[@class=\"sum\"]")).getText());
-        System.out.println(programFor15 + " " + programFor20 + " " + programFor25);
+        //System.out.println(Double.valueOf($(By.cssSelector("table[id=\"table-result-hotels-wo-margin-we\"] tbody tr td[1]")).getValue()));
+        //System.out.println(new BigDecimal(priceDBLD).setScale(0, RoundingMode.HALF_UP).floatValue());
 
-
-        //System.out.println($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[1]//span[@class=\"sum\"]")).getText());
-        programFor15 = programFor15 +
-                Double.valueOf($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][3]//td[@class=\"featureds\"]//tbody//tr[1]//span[@class=\"sum\"]")).getText());
-
-        //System.out.println($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[2]//span[@class=\"sum\"]")).getText());
-        programFor20 = programFor20 +
-                Double.valueOf($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][3]//td[@class=\"featureds\"]//tbody//tr[2]//span[@class=\"sum\"]")).getText());
-
-        //System.out.println($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][1]//td[@class=\"featureds\"]//tbody//tr[3]//span[@class=\"sum\"]")).getText());
-        programFor25 = programFor25 +
-                Double.valueOf($(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tr[@class=\"service\"][3]//td[@class=\"featureds\"]//tbody//tr[3]//span[@class=\"sum\"]")).getText());
-        System.out.println(programFor15 + " " + programFor20 + " " + programFor25);
-
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][1]//tfoot//a[@class=\"qbtn qbtn-hideallprices\"]")).click();
+        Double hotelsWE15wom = 0.0;
+        hotelsWE15wom = priceDBLD;
+        //hotelsWE15wom = hotelsWE15wom / rubUsd;
+       // hotelsWE15wom = hotelsWE15wom / generalMarge;
+        System.out.println("Hotels WE w/om 15: " + (new BigDecimal(hotelsWE15wom).setScale(0, RoundingMode.HALF_UP).floatValue()));
 
 
+        Double hotelsWE15 = 0.0;
+        hotelsWE15 = priceDBLD;
+        hotelsWE15 = hotelsWE15 / rubUsd;
+        hotelsWE15 = hotelsWE15 / generalMarge;
+        System.out.println("Hotels WE 15: " + (new BigDecimal(hotelsWE15).setScale(0, RoundingMode.HALF_UP).floatValue()));
 
 
-        /*$(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][2]//tfoot//a[@class=\"qbtn qbtn-showallprices\"]")).scrollTo();
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][2]//tfoot//a[@class=\"qbtn qbtn-showallprices\"]")).click();
-
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][2]//tfoot//a[@class=\"qbtn qbtn-hideallprices\"]")).scrollTo();
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][2]//tfoot//a[@class=\"qbtn qbtn-hideallprices\"]")).click();
-
-
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][3]//tfoot//a[@class=\"qbtn qbtn-showallprices\"]")).scrollTo();
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][3]//tfoot//a[@class=\"qbtn qbtn-showallprices\"]")).click();
-
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][3]//tfoot//a[@class=\"qbtn qbtn-hideallprices\"]")).scrollTo();
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][3]//tfoot//a[@class=\"qbtn qbtn-hideallprices\"]")).click();
-
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][4]//tfoot//a[@class=\"qbtn qbtn-showallprices\"]")).scrollTo();
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][4]//tfoot//a[@class=\"qbtn qbtn-showallprices\"]")).click();
-
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][4]//tfoot//a[@class=\"qbtn qbtn-hideallprices\"]")).scrollTo();
-        $(By.xpath("//div[@id=\"program\"]//div[@class=\"day\"][4]//tfoot//a[@class=\"qbtn qbtn-hideallprices\"]")).click();*/
+        Double services15 = 0.0;
+        services15 = programFor15;
+        services15 = services15 / rubUsd;
+        services15 = services15 / generalMarge;
+        System.out.println("Services WE w/om 15: " + (new BigDecimal(services15).setScale(0, RoundingMode.HALF_UP).floatValue()));
 
 
+        Double totalWE15 = 0.0;
+        totalWE15 = priceDBLD + programFor15;
+        totalWE15 = totalWE15 / rubUsd;
+        totalWE15 = totalWE15 / generalMarge;
+        System.out.println("Total WE 15: " + (new BigDecimal(totalWE15).setScale(0, RoundingMode.HALF_UP).floatValue()));
 
-        //
+        Double totalWE20 = 0.0;
+        totalWE20 = priceDBLD + programFor20;
+        totalWE20 = totalWE20 / rubUsd;
+        totalWE20 = totalWE20 / generalMarge;
+        System.out.println("Total WE 20: " + new BigDecimal(totalWE20).setScale(0, RoundingMode.HALF_UP).floatValue());
 
-        //$();
-
-        //$(By.id("qbtn-execute")).click();
+        Double totalWE25 = 0.0;
+        totalWE25 = priceDBLD + programFor20;
+        totalWE25 = totalWE25 / rubUsd;
+        totalWE25 = totalWE25 / generalMarge;
+        System.out.println("Total WE 35: " + new BigDecimal(totalWE25).setScale(0, RoundingMode.HALF_UP).floatValue());
 
 
     }
--- src/test/java/com/rfqDemoOltatravel/NewQuotationPage.java
+++ src/test/java/com/rfqDemoOltatravel/NewQuotationPage.java
@@ -30,4 +30,33 @@ public class NewQuotationPage {
 
     //Попап добавления городов
     public static final String cityNameButton = "div[id=\"modal-cityselector\"] button[class=\"btn btn-default\"]";
+
+
+    //Таблица Programm
+    public static class ProgrammSection {
+
+        public static String GetADay(int dayCounter) {
+
+            String result = "";
+            result = "//div[@id=\"program\"]//div[@class=\"day\"][" + String.valueOf(dayCounter) + "]";
+
+            return result;
+        }
+
+        public static String GetSumForPeople(int peopleCounter) {
+
+            String result = "";
+            result = "//td[@class=\"featureds\"]//tbody//tr[" + String.valueOf(peopleCounter) + "]//span[@class=\"sum\"]";
+
+            return result;
+        }
+
+        public static String GetSumForUnit(int peopleCounter) {
+
+            String result = "";
+            result = "//td[@class=\"featureds\"]//tbody//tr[" + String.valueOf(peopleCounter) + "]//td[3]//span[@class=\"editable editable-featured-service-ppu pricePerUnit\"]";
+
+            return result;
+        }
+    }
 }
