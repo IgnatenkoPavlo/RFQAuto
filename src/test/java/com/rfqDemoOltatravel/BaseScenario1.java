bda995178a5bf0d02261b3e2f379ade20d016c66
Pavel <pavlo.ignatenko@gmail.com>
2017-09-29 17:58:21 +0300

    Comments to test execution added

--- src/test/java/com/rfqDemoOltatravel/BaseScenario1.java
+++ src/test/java/com/rfqDemoOltatravel/BaseScenario1.java
@@ -65,34 +65,42 @@ public class BaseScenario1 {
         waitForPageToLoad();
 
         //Вводим логин с паролем и кликаем Логин
+        System.out.println("[-] Вводим логин с паролем и кликаем Логин");
         $(By.id("username")).setValue("pavel.sales");
         $(By.id("password")).setValue("password");
         $(By.cssSelector("button[type=\"submit\"]")).click();
 
         //Ждём пока загрузится страница и проподёт "Loading..."
         waitForPageToLoad();
+        System.out.println("[-] Ждём прогрузку...");
         $(By.xpath("//span[contains(text(),'Loading')]")).shouldNot(exist);
 
 
         //Открываем Quotation приложение
+        System.out.println("[-] Открываем Quotation приложение");
         open("http://rfq-demo.oltatravel.com/application/olta.quotation");
         //Ждём пока загрузится страница и проподёт "Loading..."
         waitForPageToLoad();
         $(By.xpath("//span[contains(text(),'Loading')]")).shouldNot(exist);
 
         //Ждём доступности "Create New Quotation"
+        System.out.println("[-] Ждём доступности кнопки Create New Quotation");
         $(By.id("qbtn-create")).isDisplayed();
 
         //Создаём новый Quotation
+        System.out.println("[-] Создаём новый Quotation:");
         $(By.id("qbtn-create")).click();
         driver.switchTo().alert().sendKeys("PTestQuotation1");
         driver.switchTo().alert().accept();
+        System.out.println("   Имя - PTestQuotation1");
         driver.switchTo().alert().sendKeys("PTestClient1");
         driver.switchTo().alert().accept();
+        System.out.println("   Клиент - PTestClient1");
 
         NewQuotationPage newQuotationPage = new NewQuotationPage();
 
         //Ждём пока страница прогрузится
+        System.out.println("[-] Ждём прогрузку...");
         $(By.xpath("//span[contains(text(),'Loading')]")).shouldNot(exist);
 
         Double rubUsd = 0.0;
@@ -105,19 +113,24 @@ public class BaseScenario1 {
 
 
         //Меняем колличество ночей на 3
+        System.out.println("[-] Меняем количество ночей на 3");
         $(By.cssSelector("table[id=\"table-options\"] tr[data-key=\"number_of_nights\"] td[class=\"value editable editable-quotatoin-option-value\"]")).click();
         $(By.cssSelector("table[id=\"table-options\"] tr[data-key=\"number_of_nights\"] td[class=\"value editable editable-quotatoin-option-value\"]")).setValue("3");
         $(By.cssSelector("table[id=\"table-options\"] tr[data-key=\"number_of_nights\"] td[class=\"value editable editable-quotatoin-option-value\"]")).pressEnter();
+
         $(By.xpath("//span[contains(text(),'Loading')]")).shouldNot(exist);
+        System.out.println("[-] Ждём прогрузку...");
 
         //Добавляем новую дату, дата берётся "сегодня"
+        //Получаем текущую дату
+        Date nowDate = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd-MM-yyyy");
+        System.out.println("[-] Добавляем новую дату: " + formatForDateNow.format(nowDate));
         //Кликаем на кнопку Add
         $(By.cssSelector(DatesPeriodsTable.addButton)).click();
         //Кликаем на поле для ввода даты
         $(By.cssSelector(DatesPeriodsTable.newDateInputField)).click();
-        //Получаем текущую дату
-        Date nowDate = new Date();
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd-MM-yyyy");
+
         //System.out.println("Текущая дата: " + formatForDateNow.format(nowDate));
         //Вводим дату в поле
         $(By.cssSelector(DatesPeriodsTable.newDateInputField)).setValue(formatForDateNow.format(nowDate));
@@ -127,6 +140,7 @@ public class BaseScenario1 {
         //$$(By.cssSelector("table[id=\"table-groups\"]"));
 
         //Добавляем новый Город
+        System.out.println("[-] Добавляем город: MSK");
         //Кликаем Add
         $(By.cssSelector(AccomodationsTable.addButton)).click();
         //Ждём появления меню
@@ -135,11 +149,14 @@ public class BaseScenario1 {
         $(By.cssSelector(newQuotationPage.cityNameButton)).isDisplayed();
         //Кликаем по кнопке с MSK
         $(By.cssSelector(newQuotationPage.cityNameButton)).shouldHave(text("MSK")).click();
+
         //Ждём пока страница прогрузится
+        System.out.println("[-] Ждём прогрузку...");
         $(By.xpath("//span[contains(text(),'Loading')]")).shouldNot(exist);
 
 
         //Считаем суммы для проверки
+        System.out.println("[-] Считаваем поле Sum в столбце Price DBL");
         //Кликаем на кнопку Show All Prices
         $(By.cssSelector(AccomodationsTable.showAllPricesButton)).click();
        
@@ -163,6 +180,7 @@ public class BaseScenario1 {
         Double programFor25 = 0.0;
 
         //Считаем суммы для 3-х групп: 15, 20, 25
+        System.out.println("[-] Считаем суммы для 3-х групп: 15, 20, 25");
         for (int dayCounter = 1; dayCounter <= 4; dayCounter++) {
 
             $(By.xpath(ProgrammSection.GetADay(dayCounter) + "//tfoot//a[@class=\"qbtn qbtn-showallprices\"]")).scrollTo();
@@ -191,13 +209,16 @@ public class BaseScenario1 {
 
 
         //Запускаем Расчёт
+        System.out.println("[-] Запускаем Расчёт");
         $(By.id("qbtn-execute")).scrollTo();
         $(By.id("qbtn-execute")).click();
         $(By.xpath("//span[contains(text(),'Loading')]")).shouldNot(exist);
+        System.out.println("[-] Ждём прогрузку...");
 
         //Сравниваем цену за номер
         /*Assert.assertEquals(new BigDecimal(priceDBLD).setScale(0, RoundingMode.HALF_UP).floatValue(),
                 );*/
+        System.out.println("[-] Проверяем результаты расчёта:");
         $(By.id("table-result-hotels-wo-margin-we")).scrollTo();
 
         String hotelsWE15womS = $(By.cssSelector("table[id=\"table-result-hotels-wo-margin-we\"] tbody tr td")).getText();
@@ -206,9 +227,9 @@ public class BaseScenario1 {
         String priceDBLDS = String.valueOf((int) new BigDecimal(priceDBLD).setScale(0, RoundingMode.HALF_UP).floatValue());
         //Assert.assertEquals(priceDBLDS, hotelsWE15womS);
         if(priceDBLDS.equals(hotelsWE15womS)) {
-            System.out.println("Таблица Hotels (WE) w/o margin содержит верные значения +");
+            System.out.println("    Таблица Hotels (WE) w/o margin содержит верные значения +");
         }
-        else System.out.println("Таблица Hotels (WE) w/o margin содержит неверные значения: "
+        else System.out.println("    Таблица Hotels (WE) w/o margin содержит неверные значения: "
                 + priceDBLDS + " не равен " + hotelsWE15womS + "-");
 
 
@@ -227,9 +248,9 @@ public class BaseScenario1 {
         String hotelsWER = $(By.xpath("//div[@id=\"result\"]//table[@id=\"table-result-hotels-we\"]//tbody//tr//th/following-sibling::td[1]")).getText();
         hotelsWER = hotelsWER.substring(1, hotelsWER.length());
         if(hotelsWES.equals(hotelsWER)) {
-            System.out.println("Таблица Hotels (WE) содержит верные значения +");
+            System.out.println("    Таблица Hotels (WE) содержит верные значения +");
         }
-        else System.out.println("Таблица Hotels (WE) содержит неверные значения: "
+        else System.out.println("    Таблица Hotels (WE) содержит неверные значения: "
                 + hotelsWES + " не равен " + hotelsWER + "-");
 
 
@@ -242,9 +263,9 @@ public class BaseScenario1 {
         String services15S = $(By.xpath("//div[@id=\"result\"]//table[@id=\"table-result-services\"]//tbody//tr//th/following-sibling::td[1]")).getText();
         services15S = services15S.substring(1, services15S.length());
         if(services15S.equals(String.valueOf((int) new BigDecimal(services15).setScale(0, RoundingMode.HALF_UP).floatValue()))) {
-            System.out.println("Таблица Services содержит верные значения +");
+            System.out.println("    Таблица Services содержит верные значения +");
         }
-        else System.out.println("Таблица Services содержит неверные значения: "
+        else System.out.println("    Таблица Services содержит неверные значения: "
                 + services15S + " не равен " + String.valueOf((int) new BigDecimal(services15).setScale(0, RoundingMode.HALF_UP).floatValue()) + "-");
 
 
@@ -258,14 +279,14 @@ public class BaseScenario1 {
         String totalWE15S = $(By.xpath("//div[@id=\"result\"]//table[@id=\"table-result-totals\"]//tbody//tr//th/following-sibling::td[1]")).getText();
         totalWE15S = totalWE15S.substring(1, totalWE15S.length());
         if(totalWE15S.equals(String.valueOf((int) new BigDecimal(totalWE15).setScale(0, RoundingMode.HALF_UP).floatValue()))) {
-            System.out.println("Таблица Totals (WE) содержит верные значения +");
+            System.out.println("    Таблица Totals (WE) содержит верные значения +");
         }
-        else System.out.println("Таблица Totals (WE) содержит неверные значения: "
+        else System.out.println("    Таблица Totals (WE) содержит неверные значения: "
                 + totalWE15S + " не равен " + String.valueOf((int) new BigDecimal(totalWE15).setScale(0, RoundingMode.HALF_UP).floatValue()) + "-");
 
 
         /*Double totalWE20 = 0.0;
-        totalWE20 = priceDBLD + programFor20;jhjh
+        totalWE20 = priceDBLD + programFor20;
         totalWE20 = totalWE20 / rubUsd;
         totalWE20 = totalWE20 / generalMarge;
         System.out.println("Total WE 20: " + new BigDecimal(totalWE20).setScale(0, RoundingMode.HALF_UP).floatValue());
